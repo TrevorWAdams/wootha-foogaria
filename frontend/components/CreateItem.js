c89bc1ea0b8b798f7411d3a95db080369acf9a75
Trevor W Adams <trevorwadams@Trevors-MacBook-Pro.local>
2018-11-03 11:07:12 -0600

    commit

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -45,9 +45,6 @@ class CreateItem extends Component {
     const data = new FormData();
     data.append('file', files[0]);
     data.append('upload_preset', 'foogaria-store');
-
-    //TODO: Move cloudinary variables to env or config
-
     const res = await fetch(
       'https://api.cloudinary.com/v1_1/trevoradams-com/image/upload',
       {
